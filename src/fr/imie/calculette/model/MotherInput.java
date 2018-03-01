d0f1b75e0d101be11f8526248a0dc67d04e0cfe3
PVNDORE <hiroshitakana@hotmail.fr>
2018-03-01 12:25:05 +0100

    checkOperator method complet

--- src/fr/imie/calculette/model/MotherInput.java
+++ src/fr/imie/calculette/model/MotherInput.java
@@ -1,13 +1,75 @@
 package fr.imie.calculette.model;
 
 abstract public class MotherInput {
-	
-	public void checkOperator() {
-		
+
+	public int checkOperator(String toCheck) {
+
+		int result = 0;
+
+		switch (toCheck) {
+		case "+":
+			result = 1;
+			break;
+		case "-":
+			result = 2;
+			break;
+		case "*":
+			result = 3;
+			break;
+		case ")(":
+			result = 3;
+			break;
+		case "/":
+			result = 4;
+			break;
+		case "%":
+			result = 5;
+			break;
+		case "^":
+			result = 6;
+			break;
+		case "sqrt":
+			result = 7;
+			break;
+		case "*10^":
+			result = 8;
+			break;
+		case "!":
+			result = 9;
+			break;
+		case "cos":
+			result = 10;
+			break;
+		case "sin":
+			result = 11;
+			break;
+		case "log":
+			result = 12;
+			break;
+		case "ln":
+			result = 13;
+			break;
+		case "e":
+			result = 14;
+			break;
+		case "||":
+			result = 15;
+			break;
+		case "tang":
+			result = 16;
+			break;
+		case "pi":
+			result = 17;
+			break;
+		default:
+			result = 18;
+		}
+
+		return result;
 	}
-	
+
 	public void calc() {
-		
+
 	}
 
 }
