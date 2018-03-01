53cc41dc966784c15f314d9c6755c6be142de69a
paulfazilleau <paul.fazillea@gmail.com>
2018-03-01 11:23:26 +0100

    save

--- src/fr/imie/calculette/test/test.java
+++ src/fr/imie/calculette/test/test.java
@@ -1,5 +1,17 @@
 package fr.imie.calculette.test;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener; 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class test {
 
 }
