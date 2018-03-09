7b33708e14d2b3bc340336f6bd8e1719f9898cc3
paulfazilleau <paul.fazillea@gmail.com>
2018-03-09 15:14:08 +0100

    finish test

--- src/fr/imie/calculette/test/test.java
+++ src/fr/imie/calculette/test/test.java
@@ -1,34 +1,58 @@
 package fr.imie.calculette.test;
 
 public class test {
-	
-	public static void main(String[] args) {		
-		
-		// get two double numbers
-		double cos1 = 90.0;
-		double sin1 = 180.0;
-		double tan1 = 180.0;
-		
-		//stock the printvariable
-		int Cos1 = (int) cos1;
-		int Sin1 = (int) sin1;
-		int Tan1 = (int) tan1;
-		
-		// convert them to radians
-		cos1 = Math.toRadians(cos1);
-		sin1 = Math.toRadians(sin1);
-		tan1 = Math.toRadians(tan1);
-		
-		// print their cosine
-		System.out.println("Le Cosinus de (" + Cos1 + ") = " + Math.cos(cos1));
-		System.out.println("Le Sinus de (" + Sin1 + ") = " + Math.sin(sin1));
-		System.out.println("La Tangente de (" + Tan1 + ") = " + Math.tan(tan1));
-		
-		
-		double degrees = 90 ;
-		
-		degrees = Math.cos(Math.toRadians(degrees));
-		System.out.println(degrees);
-		
-	}
+
+		public static double cos(double val) { //10
+			double cos = val, fin;
+			cos = Math.toRadians(cos);
+			fin = Math.cos(cos);
+			return fin;
+		}
+		public static double sin(double val) { //11
+			double fin, sin = val;
+			sin = Math.toRadians(sin);
+			fin = Math.sin(sin);
+			return fin;
+		}
+		public static double tan(double val) { //16
+			double tan = val, fin;
+			tan = Math.toRadians(tan);
+			fin = Math.tan(tan);
+			return fin;
+		}
+		public static double arccos(double val) { //20
+			double aCos = val, fin;
+			aCos = Math.toRadians(aCos);
+			fin = Math.acos(aCos);
+			return fin;
+		}
+		public static double arcsin(double val) { //21
+			double fin, aSin = val;
+			aSin = Math.toRadians(aSin);
+			fin = Math.asin(aSin);
+			return fin;
+		}
+		public static double arctan(double val) { //22
+			double aTan = val, fin;
+			aTan = Math.toRadians(aTan);
+			fin = Math.atan(aTan);
+			return fin;
+		}
+		public static double log(double val) { //12
+			double fin = Math.log(val)/Math.log(10);
+			return fin;
+		}
+		public static double ln(double val) { //13
+			double fin = Math.log(val);
+			return fin;
+		}
+		public static double exponentielle(double val) { //14
+			double fin = 0;
+			fin = Math.exp(val);
+			return fin;
+		}
+		public static double pi() { //17
+			double fin = Math.PI;
+			return fin;
+		}
 }
